be7884ab337849f66e9f2de35a638d2117630f3a
Abeera Tahir <abeeratahir2018@gmail.com>
2023-03-28 19:07:26 +0500

    Fix syntax in view and add data in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create([
+  { message: 'Hello' },
+  { message: 'Hi' },
+  { message: 'Hey' },
+  { message: 'Hola' },
+  { message: 'What up' }
+])
